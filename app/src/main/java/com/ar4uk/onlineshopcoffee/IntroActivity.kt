69b870bf272aeb005a187a7c3b40903d151e1856
a.galeev <a.galeev@malltech-lab.ru>
2024-09-22 22:03:18 +0500

    add intro activity

---
+++ app/src/main/java/com/ar4uk/onlineshopcoffee/IntroActivity.kt
@@ -0,0 +1,26 @@
+package com.ar4uk.onlineshopcoffee
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ar4uk.onlineshopcoffee.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+    }
+}
